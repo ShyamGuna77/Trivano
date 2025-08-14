33200d31f2450e6857cf71d9637541abd288c48e
Shyam <gshyamprasad2001@gmail.com>
2025-08-14 07:02:04 +0530

    initial

---
+++ src/lib/SiteDetails.tsx
@@ -0,0 +1,18 @@
+
+
+
+export const SiteDetails = {
+  name: "Trivano",
+  description:
+    "A intuitive analytics platform designed to give you crystal-clear insights",
+
+  image: "https://trivano.com/image.png",
+  keywords: ["trivano", "analytics", "platform"],
+  url: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+  author: "SinisterSpD",
+  copyright: "SinisterSpD - All Rights Reserved",
+  license: "MIT",
+  version: "1.0.0",
+};
+
+
